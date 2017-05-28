3d3c1e3103777dded3014387ddcff19d375f9e27
admin <admin@admin-PC>
2017-05-28 17:01:21 +0530

    this eclipse project commite

---
+++ MyGitProject/src/com/git/MygitClass.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class MygitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("we are into git workshop");
+
+	}
+
+}
