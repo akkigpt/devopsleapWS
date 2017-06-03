781412cd1effb9c7767f5c60a8b30855b8b8bedc
akkigpt <akshay.gniet1000@gmail.com>
2017-06-03 17:35:57 +0530

    Update MygitClass.java

--- MyGitProject/src/com/git/MygitClass.java
+++ MyGitProject/src/com/git/MygitClass.java
@@ -5,7 +5,7 @@ public class MygitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("we are into git workshop");
+		System.out.print("we are intoffffffffffff git workshop");
 		
 		System.out.print("no corrct code");
 
